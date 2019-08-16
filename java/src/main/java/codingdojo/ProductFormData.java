b151f139f1e45abce55144212ac244bd0fc9cc1b
Gregor Riegler <gregor.riegler@ebcont.com>
2019-08-16 23:44:15 +0200

    first try

--- java/src/main/java/codingdojo/ProductFormData.java
+++ java/src/main/java/codingdojo/ProductFormData.java
@@ -1,5 +1,8 @@
 package codingdojo;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 /**
  * Received from the client with all the details of the new product to be added to the catalogue.
  */
@@ -10,9 +13,7 @@ public class ProductFormData {
     private final double suggestedPrice;
     private final boolean packagingRecyclable;
 
-    public ProductFormData(String name, String type, double weight,
-                           double suggestedPrice, boolean packagingRecyclable) {
-
+    public ProductFormData(String name, String type, double weight, double suggestedPrice, boolean packagingRecyclable) {
         this.name = name;
         this.type = type;
         this.weight = weight;
@@ -20,23 +21,69 @@ public class ProductFormData {
         this.packagingRecyclable = packagingRecyclable;
     }
 
-    public String getType() {
-        return type;
+    Optional<Response> getValidationFailure() {
+        if ("".equals(name)) {
+            return Optional.of(new Response(0, -2, "Missing Name"));
+        } else if (isType("")) {
+            return Optional.of(new Response(0, -2, "Missing Type"));
+        } else if (weight < 0) {
+            return Optional.of(new Response(0, -3, "Weight error"));
+        } else if (isType("Blusher") && weight > 10) {
+            return Optional.of(new Response(0, -3, "Error - weight too high"));
+        } else if (isType("Lipstick") && suggestedPrice > 20 && (weight > 0 && weight < 10 || !packagingRecyclable)) {
+            return Optional.of(new Response(0, -1, "Error - failed quality check for Queen Range"));
+        } else if (isType("Unknown")) {
+            return Optional.of(new Response(0, -1, "Unknown product type " + type));
+        } else {
+            return Optional.empty();
+        }
     }
 
-    public String getName() {
-        return name;
+    Product toProduct() {
+        Product product = new Product(name);
+        product.setFamily(getProductFamily());
+        product.setType(getProductType());
+        product.setRange(getProductRange());
+        product.setWeight(weight);
+        return product;
     }
 
-    public double getWeight() {
-        return weight;
+    private boolean isType(String type) {
+        return type.equals(this.type);
     }
 
-    public double getSuggestedPrice() {
-        return suggestedPrice;
+    private ProductFamily getProductFamily() {
+        if (isType("Eyeshadow")) {
+            return ProductFamily.EYES;
+        } else if (isType("Lipstick")) {
+            return ProductFamily.LIPS;
+        } else if (isType("Mascara")) {
+            return ProductFamily.LASHES;
+        } else if (isType("Blusher") || isType("Foundation")) {
+            return ProductFamily.SKIN;
+        }
+        return null;
     }
 
-    public boolean isPackagingRecyclable() {
-        return packagingRecyclable;
+    private String getProductType() {
+        if (Stream.of("Eyeshadow", "Mascara", "Lipstick", "Blusher", "Foundation").anyMatch(s -> isType(s))) {
+            return type;
+        } else {
+            return "Unknown";
+        }
+    }
+
+    private ProductRange getProductRange() {
+        if (isType("Lipstick") && suggestedPrice > 20
+            || isType("Mascara") && suggestedPrice > 25 && packagingRecyclable) {
+            return ProductRange.QUEEN;
+        } else if (packagingRecyclable
+            || isType("Foundation") && suggestedPrice > 10
+            || isType("Lipstick") && suggestedPrice > 10
+            || isType("Mascara") && suggestedPrice > 15) {
+            return ProductRange.PROFESSIONAL;
+        } else {
+            return ProductRange.BUDGET;
+        }
     }
 }
--- java/src/main/java/codingdojo/ProductService.java
+++ java/src/main/java/codingdojo/ProductService.java
@@ -13,77 +13,13 @@ public class ProductService {
      * If it is correct, add the new product to the database.
      */
     public Response validateAndAdd(ProductFormData productData) {
-        if ("".equals(productData.getName())) {
-            return new Response(0, -2, "Missing Name");
-        }
-        if ("".equals(productData.getType()) ) {
-            return new Response(0, -2, "Missing Type");
-        }
-        Product product = new Product(productData.getName());
-        product.setType("Unknown");
-
-        if ("Eyeshadow".equals(productData.getType()) || "Mascara".equals(productData.getType())) {
-            product.setType(productData.getType());
-            product.setFamily(ProductFamily.EYES);
-            if ("Eyeshadow".equals(productData.getType()) && product.getName().contains("Queen")) {
-                product.setRange(ProductRange.QUEEN);
-            }
-        }
-        product.setRange(ProductRange.BUDGET);
-        if (productData.isPackagingRecyclable()) {
-            product.setRange(ProductRange.PROFESSIONAL);
-        }
-        if ("Foundation".equals(productData.getType())) {
-            if (productData.getSuggestedPrice() > 10) {
-                product.setRange(ProductRange.PROFESSIONAL);
-            }
-        }
-
-        if ("Lipstick".equals(productData.getType())) {
-            product.setType(productData.getType());
-            product.setFamily(ProductFamily.LIPS);
-            if (productData.getSuggestedPrice() > 10) {
-                product.setRange(ProductRange.PROFESSIONAL);
-            }
-            if (productData.getSuggestedPrice() > 20) {
-                if (productData.getWeight() > 0 && productData.getWeight() < 10) {
-                    return new Response(0, -1, "Error - failed quality check for Queen Range");
-                }
-                product.setRange(ProductRange.QUEEN);
-            }
-        }
-
-        if ("Mascara".equals(productData.getType())) {
-            product.setFamily(ProductFamily.LASHES);
-            if (productData.getSuggestedPrice() > 15) {
-                product.setRange(ProductRange.PROFESSIONAL);
-            }
-            if (productData.getSuggestedPrice() > 25 && productData.isPackagingRecyclable()) {
-                product.setRange(ProductRange.QUEEN);
-            }
-        }
-
-        if (productData.getWeight() < 0) {
-            return new Response(0, -3, "Weight error");
-        }
-        product.setWeight(productData.getWeight());
-
-        if ("Blusher".equals(productData.getType()) || "Foundation".equals(productData.getType())) {
-            product.setType(productData.getType());
-            product.setFamily(ProductFamily.SKIN);
-            if ("Blusher".equals(productData.getType()) && productData.getWeight() > 10) {
-                return new Response(0, -3, "Error - weight too high");
-            }
-        }
-
-        if (!productData.isPackagingRecyclable() && product.getRange() == ProductRange.QUEEN) {
-            return new Response(0, -1, "Error - failed quality check for Queen Range");
-        }
-
-        if ("Unknown".equals(product.getType())) {
-            return new Response(0, -1, "Unknown product type " + productData.getType());
-        }
-
-        return new Response(db.storeProduct(product), 0, "Product Successfully Added");
+        return productData.getValidationFailure()
+            .orElseGet(() -> storeProduct(productData.toProduct()));
     }
+
+    private Response storeProduct(Product product) {
+        int productId = db.storeProduct(product);
+        return new Response(productId, 0, "Product Successfully Added");
+    }
+
 }
